a0aa381b64fb48d86213d75a7208354b34282f27
Альтаир фон Альфхейм <insania_officialis@outlook.com>
2025-09-09 15:56:51 +0300

    - Добавление миграции кода статуса в логи

---
+++ Insania.Politics.Database/Migrations/LogsApiPolitics/20250909125458_Add_Status-Code-In-Log_0.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Insania.Politics.Database.Migrations.LogsApiPolitics
+{
+    /// <inheritdoc />
+    public partial class Add_StatusCodeInLog_0 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "status_code",
+                schema: "insania_logs_api_politics",
+                table: "r_logs_api_politics",
+                type: "integer",
+                nullable: true,
+                comment: "Код статуса");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status_code",
+                schema: "insania_logs_api_politics",
+                table: "r_logs_api_politics");
+        }
+    }
+}
